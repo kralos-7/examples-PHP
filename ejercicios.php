0a6e3b7ab55a4926862e890747e6f29868b4e5e7
Erik <vasquezerik611@gmail.com>
2023-10-18 16:09:28 -0600

    actualización documento ejercicios.php
    integración de ejercicios a un archivo php

--- ejercicios.php
+++ ejercicios.php
@@ -1,36 +1,42 @@
 <?php
-
 /**
  * 1. Muestra los números del 1 al 10
  */
-echo "1. Muestra los números del 1 al 10";
-echo "/n";
+echo "1. Muestra los números del 1 al 10\n";
 echo "Tabla del 1 por 1:\n";
 for ($i = 1; $i <= 10; $i++) {
     echo "1 * $i = " . (1 * $i) . "\n";
 }
-echo "/n";
+echo "\n";
+?>
+<?php
 /**
  * 2. Poner un hola mundo dentro de un html.
  */
-echo "2. Poner un hola mundo dentro de un html";
-echo "/n";
-echo "<h1>Hola Mundo desde PHP</h1>";
-echo "/n";
+echo "2. Poner un hola mundo dentro de un html\n";
+echo "<h1>Hola Mundo desde PHP</h1>\n";
+?>
+<?php
 /**
  * 3. POner una imagen en un img con la ruta de la imagen.
  */
-
+?>
+<?php
 /**
  * 4. Crear un select en HTML y poner los números del 1 al 10 como opciones.
  */
-
+echo "\n";
+for ($i = 1; $i <= 10; $i++) {
+    echo "<option value=\"$i\">$i</option>";
+}
+echo "\n";
+?>
+<?php
 /**
  * 5. Muestra los números del 1 al 10
  * 
  */
-echo "5. Muestra los números del 1 al 10";
-echo "/n";
+echo "5. Muestra los números del 1 al 10\n";
 echo "Tablas de multiplicar:\n";
 for ($i = 1; $i <= 10; $i++) {
     for ($j = 1; $j <= 10; $j++) {
@@ -38,3 +44,4 @@ for ($i = 1; $i <= 10; $i++) {
     }
     echo "\n";
 }
+?>
